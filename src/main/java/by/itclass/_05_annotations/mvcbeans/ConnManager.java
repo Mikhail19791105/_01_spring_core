19b6ff0402517711f6efc94f0ac0345bd87cafcd
Mikhail19791105 <acme.ac58@gmail.com>
2025-04-09 16:41:02 +0300

    spring 02

---
+++ src/main/java/by/itclass/_05_annotations/mvcbeans/ConnManager.java
@@ -0,0 +1,12 @@
+package by.itclass._05_annotations.mvcbeans;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConnManager {
+    @Override
+    public String toString() {
+        return "ConnManager class";
+    }
+}
---
+++ src/main/java/by/itclass/_05_annotations/mvcbeans/Controller.java
@@ -0,0 +1,20 @@
+package by.itclass._05_annotations.mvcbeans;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Controller {
+    private Service service;
+
+    @Autowired
+    public void setService(Service service) {
+        this.service = service;
+    }
+
+    @Override
+    public String toString() {
+        return "Controller class, Service: " + service;
+    }
+}
